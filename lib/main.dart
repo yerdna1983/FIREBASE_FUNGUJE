e1be65a7ece22e6644f13582d6a6e6a774b1b0f9
FlutLab.io <>
2021-02-20 14:07:36 +0000

    Snapshot 'test'

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,73 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Baby Names',
-      home: MyHomePage(),
+      title: 'Navigation Over Screens',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+//      home: MainPage(),
+
+      // Declare routes
+      routes: {
+        // Main initial route
+        '/': (context) => MainPage(),
+        // Second route
+        '/second': (context) => SecondPage(),
+      },
+      initialRoute: '/',
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
+  Widget build(BuildContext context) =>
+      Scaffold(
+        appBar: AppBar(
+          title: Text('Navigation over screens'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              // Navigate using declared route name
+              RaisedButton(
+                onPressed: () => Navigator.pushNamed(context, '/second'),
+                child: Text('Navigate using routes'),
+              ),
+              // Navigate using simple push method
+              RaisedButton(
+                onPressed: () =>
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondPage()),
+                    ),
+                child: Text('Navigate using push method'),
+              )
+            ],
+          ),
+        ),
+      );
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Baby Name Votes')),
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
-
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final record = Record.fromSnapshot(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => record.reference.updateData({
-            'votes': FieldValue.increment(1)
-          }),
+      appBar: AppBar(
+        title: Text('Second screen'),
+      ),
+      body: Container(
+        child: RaisedButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('Back'),
         ),
       ),
     );
   }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['name'] != null),
-        assert(map['votes'] != null),
-        name = map['name'],
-        votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot) : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$votes>";
 }
